20b1bd9e581f831000260884ad3b1d6bfd60ebdf
Georgi Iliev <georgi.iliev533@outlook.com>
2023-05-14 12:41:40 +0300

    Base structure of project partially implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "Security/SHA256.h"
+#include "Utilities/MyString.h"
+#include "Users/Client.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Client az("georgi", "parola", "Georgi", "Iliev");
+//    std::cout << std::boolalpha << az.verifyPassword("parola");
+    User* something = &az;
+    if(something->getType() == UserType::Client) {
+        std::cout << "Client";
+    }
+    else if(something->getType() == UserType::Driver) {
+        std::cout << "Driver";
+    }
+
     return 0;
 }
